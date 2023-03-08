e42fe885d4907e9409ae27485f6cc6c2f13f45c3
Samartha <91981709+Samartha27@users.noreply.github.com>
2023-03-08 11:40:27 -0800

    Add files via upload

---
+++ epipolar_json/data/replace.py
@@ -0,0 +1,19 @@
+import json
+import numpy as np
+
+
+f = open('/Users/stlp/Desktop/sam/ram/pose/vis_results/yolox/S005/c028/keypoints.json', "r+")
+file_data = json.load(f)
+
+copy = file_data['annotations'].copy()
+
+for idx,frame in enumerate(copy):
+    if ('frame_id : '+str(idx)) not in file_data['annotations'][idx]:
+        dict2 = {}
+        dict2["bbox"] = np.zeros([5], dtype = float).tolist()
+        dict2["keypoints"] = np.zeros([17,3], dtype = float).tolist()
+        print(dict2)
+        file_data['annotations'][idx]["frame_id : "+str(idx)] = [dict2]
+        f.seek(0)
+        
+json.dump(file_data, f)
